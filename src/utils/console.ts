a0557e89e31d5295ad4c80985e6ce6ee13192b5e
kenneth <kennethbelardo55@gmail.com>
2024-07-19 19:10:20 +0800

    fix: Fix bug where the logging statements are not seen.
    Added console.log to ensure that the loggings statements will be shown on console.

--- src/utils/console.ts
+++ src/utils/console.ts
@@ -3,13 +3,13 @@ import chalk from "chalk";
 const header = "[routex]";
 
 export const log = (...text: unknown[]) => {
-  chalk.green(header, text);
+  console.log(chalk.green(header, text));
 };
 
 export const info = (...text: unknown[]) => {
-  chalk.blueBright(header, text);
+  console.log(chalk.blueBright(header, text));
 };
 
 export const err = (...text: unknown[]) => {
-  chalk.red(header, text);
+  console.log(chalk.red(header, text));
 };
